c409dcf1412d383da896081595b003f71fc2807d
Rennan Damião Moreira <50747648+RennanDamiaoMoreira@users.noreply.github.com>
2022-02-25 21:04:25 -0300

    final aula 41

--- estudos-de-lista/src/App.js
+++ estudos-de-lista/src/App.js
@@ -1,33 +1,26 @@
-import React, { Component} from 'react';
-
+import React, { Component } from "react";
+import  Feed  from "./components/feed";
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      feed:[
-        {id:1,username:'matheus', curtidas:10 , comentarios:2},
-        {id:2,username:'Rennan',curtidas:20, comentarios:3},
-        {id:3,username:'Aline',curtidas:20, comentarios:3},
-        {id:4,username:'ariane',curtidas:45, comentarios:4}
-      ]
-    }
+      feed: [
+        { id: 1, username: "matheus", curtidas: 10, comentarios: 2 },
+        { id: 2, username: "Rennan", curtidas: 20, comentarios: 3 },
+        { id: 3, username: "Aline", curtidas: 20, comentarios: 3 },
+        { id: 4, username: "ariane", curtidas: 45, comentarios: 4 },
+      ],
+    };
   }
 
   render() {
     return (
       <div>
-
-        {this.state.feed.map((item)=> {return (
-          <div>
-            <h3>{item.username}</h3>
-            <ul>
-              <li>Comentarios : {item.comentarios}</li>
-              <li>Curtidas : {item.curtidas}</li>
-            </ul>
-          </div>
-        )})}
+        {this.state.feed.map((item) => {
+          return <Feed atributes={item} />;
+        })}
       </div>
-    )
+    );
   }
 }
 
