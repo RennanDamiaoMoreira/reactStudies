443d6342e18b965bbd4989b19f60409a20ae3aab
Rennan Damião Moreira <50747648+RennanDamiaoMoreira@users.noreply.github.com>
2022-03-03 21:41:47 -0300

    aa

--- react_avancado/src/App.js
+++ react_avancado/src/App.js
@@ -1,36 +1,36 @@
 import React, { useEffect, useState } from "react";
 import "./style.css";
 //https://sujeitoprogramador.com/rn-api/?api=posts
-function App() {
-  const [nutri, setNutri] = useState([]);
+// function App() {
+//   const [nutri, setNutri] = useState([]);
 
-  useEffect(() => {
-    function loadApi() {
-      let url = "https://sujeitoprogramador.com/rn-api/?api=posts";
-      fetch(url)
-        .then((r) => r.json())
-        .then((json) => {
-          setNutri(json);
-        });
-    }
-    loadApi();
-  });
-  return (
-    <div className="container">
-      <header>
-        <strong>React Nutri</strong>
-      </header>
-      {nutri.map((element) => {
-        return (
-          <article key={element.id} className="post">
-            <strong className="titulo">{element.title}</strong>
-            <img src={element.capa} alt={element.titulo} className="capa"></img>
-            <p className="subtitulo">{element.subtitulo}</p>
-          </article>
-        );
-      })}
-    </div>
-  );
-}
+//   useEffect(() => {
+//     function loadApi() {
+//       let url = "https://sujeitoprogramador.com/rn-api/?api=posts";
+//       fetch(url)
+//         .then((r) => r.json())
+//         .then((json) => {
+//           setNutri(json);
+//         });
+//     }
+//     loadApi();
+//   });
+//   return (
+//     <div className="container">
+//       <header>
+//         <strong>React Nutri</strong>
+//       </header>
+//       {nutri.map((element) => {
+//         return (
+//           <article key={element.id} className="post">
+//             <strong className="titulo">{element.title}</strong>
+//             <img src={element.capa} alt={element.titulo} className="capa"></img>
+//             <p className="subtitulo">{element.subtitulo}</p>
+//           </article>
+//         );
+//       })}
+//     </div>
+//   );
+// }
 
 export default App;
