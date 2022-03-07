db359602c7a11d79af8c94d34f8e2ecedfa4279c
Rennan Damião Moreira <50747648+RennanDamiaoMoreira@users.noreply.github.com>
2022-03-07 18:45:19 -0300

    pagina home aula 58

--- filmaria/src/App.js
+++ filmaria/src/App.js
@@ -1,7 +1,7 @@
 import Routes from './routes';
 import './style.css';
 
-//https://sujeitoprogramador.com/r-api/?api=filmes
+
 function App() {
   return (
     <div className="app">
--- filmaria/src/pages/Home/index.js
+++ filmaria/src/pages/Home/index.js
@@ -1,8 +1,39 @@
+import api from "../../services/api";
+import {useEffect,useState} from 'react';
+import { Link } from "react-router-dom";
+import './style.css';
 
 export default function Home() {
+    const [films, setFilms] = useState([]);
+
+    useEffect(() => {
+        async function loadFilms(){
+            const response = await api.get('/r-api/?api=filmes');
+            //console.log(response);
+            if (response.status === 200) {
+                setFilms(response.data);
+            }else{
+                alert("erro ao invocar api code : "+ response.status)
+            }
+        }
+        loadFilms();
+    },[]);
+
     return (
-        <div className="">
-            <h1>Voce esta na home</h1>
+        <div className="container">
+           <div className="list-films">
+            {films.map(film =>{
+                return (
+                    <article key={film.id}>
+                        <strong>
+                            {film.nome}
+                        </strong>
+                        <img src={film.foto}/>
+                       <Link to={"/film/"}>Acessar</Link>
+                    </article>
+                )
+            })}
+            </div>
         </div>
     )
 }
